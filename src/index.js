67ba61c5271e7a8ce545f2c339f4f6a617c27efd
Alex Waller <alex@artofalexwaller.com>
2021-05-03 17:17:53 -0400

    imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 // react-redux, redux, thunk
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
