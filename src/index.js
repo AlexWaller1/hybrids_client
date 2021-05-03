894010758f6ddf98ba02324ef8180700bd97f004
Alex Waller <alex@artofalexwaller.com>
2021-05-03 17:33:00 -0400

    createStore

--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 
 // need to create a store for Redux => createStore 1. reducer 2. dev tools
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
