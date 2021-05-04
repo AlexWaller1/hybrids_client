da72b7852fe8598ed64ea11fe560a03a945ee61a
Alex Waller <alex@artofalexwaller.com>
2021-05-04 18:01:50 -0400

    added header

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import Router from './Router'
 
+import Header from './Header'
+
 import HybridsContainer from './HybridsContainer'
 
 const App = () => {
     return (
         <div>
+          <Header />
           <HybridsContainer />
           <Router />
         </div>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <h1>
+            ANTIBI INCORPORATED HYBRID TRACKER
+        </h1>
+    );
+};
+
+export default Header;
