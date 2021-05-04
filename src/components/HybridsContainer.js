f5e44bb2e912c446a1698e82cc73ab54811215df
Alex Waller <alex@artofalexwaller.com>
2021-05-03 21:41:46 -0400

    HybridsContainerComponent

--- src/components/HybridsContainer.js
+++ src/components/HybridsContainer.js
@@ -1,1 +1,15 @@
 // this will be our hybrids container; it will hold hybrids data and hybrids methods
+
+import React, { Component } from 'react';
+
+class HybridsContainer extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default HybridsContainer;
