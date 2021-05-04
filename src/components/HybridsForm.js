b1f422331d4d9da74de2273c1b0bd93d289bd582
Alex Waller <alex@artofalexwaller.com>
2021-05-03 23:09:40 -0400

    localFormState

--- src/components/HybridsForm.js
+++ src/components/HybridsForm.js
@@ -1,9 +1,20 @@
 // we want controlled components
 // so values of input fields will be controlled by state
+// store state is global
+// component state is local and isolated to the respective component
 
 import React, { Component } from 'react';
 
 class HybridsForm extends Component {
+
+   state = {
+       name: '',
+       species: '',
+       origin: '',
+       personality: '',
+       image: ''
+   }
+
     render() {
         return (
             <div>
