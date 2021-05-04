56cefa28b885613596ba7d72cb5d279ee4aa526d
Alex Waller <alex@artofalexwaller.com>
2021-05-04 12:39:12 -0400

    About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const About = () => {
     return (
         <div>
-           Home Page
+            This is an about page
         </div>
     );
 };
 
-export default Home;
+export default About;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
-import Home from '../components/home'
+import Home from '../components/Home'
+import HybridsContainer from './HybridsContainer'
+import About from '../components/About'
 
 const Router = () => {
     return (
