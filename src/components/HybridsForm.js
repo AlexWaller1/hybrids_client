ada285ec7fed8df614fdee65debea5c1395b7b3d
Alex Waller <alex@artofalexwaller.com>
2021-05-03 23:04:41 -0400

    HybridsForm

--- src/components/HybridsForm.js
+++ src/components/HybridsForm.js
@@ -1,2 +1,16 @@
 // we want controlled components
 // so values of input fields will be controlled by state
+
+import React, { Component } from 'react';
+
+class HybridsForm extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default HybridsForm;
