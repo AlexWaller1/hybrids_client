d28bc1d6c4fb2092a75e9298ac7e6e1148ca3c33
Alex Waller <alex@artofalexwaller.com>
2021-05-14 15:52:28 -0400

    addRobot

--- src/actions/robotsActions.js
+++ src/actions/robotsActions.js
@@ -8,3 +8,19 @@ export const fetchRobots = () => {
         .then(robots => dispatch({ type: 'FETCH_ROBOTS', payload: robots}))
     }
 }
+
+// this data will go to the robotsReducer
+// the default of fetch is to make a get request
+// if not a get request, we need to specify
+
+export const addRobot = robot => {
+    return dispatch => {
+        fetch('http://localhost:3000/robots', {
+        method: 'POST',
+        body: JSON.stringify(robot),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(resp => resp.json())
+    .then(robot => dispatch({ type: 'ADD_ROBOT', payload: robot }))
+   }
+}
