05349d94aaa44dc29458665a9ddb3c6f820ce8b5
Alex Waller <alex@artofalexwaller.com>
2021-05-19 16:00:43 -0400

    clickHandler

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -3,4 +3,19 @@ import React, { Component } from 'react';
 
 class LikeButton extends Component {
 
+    state = {
+        likes: 0
+    };
+
+    addLike = () => {
+        let newCount = this.state.likes + 1;
+        this.setState({
+            likes: newCount
+        });
+    };
+
+    render() {
+        return <button>Have Further Interest: {this.state.likes}</button>
+    }
+
 }
