a72dc13b47231e0c9da9358cacabe5e5663cfbfa
Alex Waller <alex@artofalexwaller.com>
2021-05-22 16:41:37 -0400

    MinimartsForm

---
+++ src/components/MinimartsForm.js
@@ -0,0 +1,4 @@
+// form where users can fill out Minimart information
+// form will have react state for its text fields
+// here, we can call addMinimarts from Redux actions as a prop
+// onSubmit event handler can call addMinimart prop to submit new Minimarts to Redux store
