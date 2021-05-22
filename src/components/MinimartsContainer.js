dabbd1e094ee65d8173ff3fa4c578a066946e53a
Alex Waller <alex@artofalexwaller.com>
2021-05-22 15:19:38 -0400

    componentDidMount

--- src/components/MinimartsContainer.js
+++ src/components/MinimartsContainer.js
@@ -2,7 +2,26 @@
 // here we will pull our minimarts data 
 // and fetch it for our application.
 
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { fetchMinimarts } from '../actions/minimartsActions'
+
 class MinimartsContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchMinimarts()
+        // pulls minimart data and fetches it for application
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 
 }
 
+export default connect(null, { fetchMinimarts })(MinimartsContainer);
