fddd08812270eb9ca0b297790e2d21f272681ba0
Alex Waller <alex@artofalexwaller.com>
2021-05-22 16:35:25 -0400

    MinimartsContainer

--- src/components/MinimartsContainer.js
+++ src/components/MinimartsContainer.js
@@ -12,12 +12,14 @@ class MinimartsContainer extends Component {
     componentDidMount() {
         this.props.fetchMinimarts()
         // pulls minimart data and fetches it for application
+        // componentDidMount special name for method, when MinimartsContainer mounts, it will
+        // autmomatically call the method specified
     }
 
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }
@@ -25,3 +27,10 @@ class MinimartsContainer extends Component {
 }
 
 export default connect(null, { fetchMinimarts })(MinimartsContainer);
+
+// componentDidMount special name for method, when MinimartsContainer mounts, it will
+// automatically call the method specified
+// we're not using any values from redux store, but we're using redux actions
+// react, react-redux, react-router-dom are different packages
+// react-redux allows us to pass redux values and actions to components as props
+// react-router-dom responsible for URL and links
