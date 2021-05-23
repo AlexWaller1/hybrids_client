c469b559683dfbe8e996c3c54c0dd05ad22ee930
Alex Waller <alex@artofalexwaller.com>
2021-05-22 22:26:18 -0400

    MinimartsList

--- src/components/MinimartsList.js
+++ src/components/MinimartsList.js
@@ -12,14 +12,33 @@ const MinimartsList = ({ minimarts, deleteMinimart }) => {
         <div>
             <MinimartForm />
             <ul>
+            {minimarts.map(minimart => (
+                <li key={minimart.id}>
+                     <h2> {minimart.name} </h2>
+                     <h2> {minimart.state} </h2>
+                     <h3> {minimart.address} </h3>
+                     <h4> {minimart.sodamachine} </h4>
+                     <h5> {minimart.description} </h5>
+                     <img src={minimart.image} alt='Minimart Image' />
 
+                     <button onClick={
+                         e => {
+                             deleteMinimart(minimart.id)
+                         }
+                     }>
+                         Delete
+                     </button>
+                </li>
+            ))}
             </ul>
         </div>
     );
 };
 
 const mapStateToProps = state => {
-    return { hybrids: state.hybrids }
+    return { minimarts: state.minimarts }
+    // connecting to Redux state, and then setting up a key-value pair. The key can be called
+    // in our JSX
 }
 
 const mapDispatchToProps = dispatch => {
@@ -27,3 +46,5 @@ const mapDispatchToProps = dispatch => {
         deleteMinimart: (id) => {dispatch(removeMinimart(id))}
     }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(MinimartsList);
