0e26bee8587b638af4a285da39c8a1f51912b9b7
Alex Waller <alex@artofalexwaller.com>
2021-05-22 20:02:39 -0400

    MinimartsForm

--- src/components/MinimartsForm.js
+++ src/components/MinimartsForm.js
@@ -2,3 +2,27 @@
 // form will have react state for its text fields
 // here, we can call addMinimarts from Redux actions as a prop
 // onSubmit event handler can call addMinimart prop to submit new Minimarts to Redux store
+
+// we want controlled components
+// so values of input fields will be controlled by state
+// store state is global
+// component state is local and isolated to the respective component
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import { addMinimart } from '../actions/minimartsActions'
+
+class MinimartsForm extends Component {
+
+    state = {
+        name: '',
+        address: '',
+        sodamachine: '',
+        description: '',
+        state: '',
+        image: ''
+    }
+
+    
+}
