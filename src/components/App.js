4e6530fa185ed5c3953023d173d8242466435594
Alex Waller <alex@artofalexwaller.com>
2021-05-26 15:01:23 -0400

    objectOrientedNotes

--- src/components/App.js
+++ src/components/App.js
@@ -23,3 +23,32 @@ const App = () => {
 };
 
 export default App;
+
+/* 
+
+Before diving into the four pillars of Object Oriented Programming, let's go over some
+of the basic terminologies
+
+Object: The instance of a class/ it's the working entity of a class
+
+Class: This is the model or standard about the capability of what an object can do
+
+Method: Can modify a class state that would apply across all the instances of the class
+
+Instance: These are like Objects, however, let's think about it in these terms: A blueprint
+for a car design is the class description, all the cars manufactured from that blueprint are
+objects of that class. Your car that has been made from that blueprint is an instance of that
+class.
+
+Now that we have covered these keywords, let's jump into the four principles of object-
+oriented-programming: Encapsulation, Abstraction, Inheritance, and Polymorphism.
+
+- The 4 Pillars of Object Oriented Programming
+
+- 1. Encapsulation
+
+     
+
+
+
+*/
