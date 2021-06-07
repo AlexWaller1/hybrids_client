b752ed1869b75c19b5e883721b1e5328e06a2719
Alex Waller <alex@artofalexwaller.com>
2021-06-07 19:56:55 -0400

    hybrid show page

--- src/components/SingleHybrid.js
+++ src/components/SingleHybrid.js
@@ -6,5 +6,27 @@ export const SingleHybridPage = ({ match }) => {
     const { hybridId } = match.params
 
     const hybrid = useSelector(state => 
-        state.hybrids.find(hybrid => hybrid.id === hybridId))
+        state.hybrids.find(hybrid => hybrid.id === hybridId)
+        )
+
+if (!hybrid) {
+    return (
+        <section>
+            <h2>Hybrid Not in Database.</h2>
+        </section>
+    )
+}
+
+return (
+    <section>
+        <article className="hybrid">
+            <h2> {hybrid.name} </h2>
+            <h3> {hybrid.species} </h3>
+            <h4> {hybrid.origin} </h4>
+            <h5> {hybrid.personality} </h5>
+            <img src={hybrid.image} alt='Hybrid Image'/>
+
+        </article>
+    </section>
+)
 }
