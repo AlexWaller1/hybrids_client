9701cef807e6df8f8bf24193bc0a4f65acc125ec
Alex Waller <alex@artofalexwaller.com>
2021-06-09 18:03:10 -0400

    MotorhomesContainer

---
+++ src/components/MotorhomesContainer.js
@@ -0,0 +1,25 @@
+// This component will be called when application is launched
+// while holding no other components, it will be responsible for calling the get fetch function
+
+import React, { Component } from 'react'
+// always have to import react, have to import { Component } if we are using a class component
+import { connect } from 'react-redux'
+// have to use connect if we are connecting to the Redux store
+import { fetchMotorhomes } from '../actions/motorhomesActions'
+// also have to import functions from Redux that we would like to use
+
+class MotorhomesContainer extends Component {
+
+    componentDidMount(){
+        this.props.fetchMotorhomes
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default connect(null, {fetchMotorhomes})(MotorhomesContainer);
