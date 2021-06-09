cb2a9cf8068b89964a3dcdd31e0fcc793eeaccf4
Alex Waller <alex@artofalexwaller.com>
2021-06-09 11:51:59 -0400

    CitiesList

--- src/components/CitiesList.js
+++ src/components/CitiesList.js
@@ -14,6 +14,44 @@ const CitiesList = ({ cities, deleteCity }) => {
     return (
         <div>
             <CitiesForm />
+
+            <ul>
+                {cities.map(city => (
+                    <li key={city.id}>
+                        <h2> {city.name} </h2>
+                        <h3> {city.population} </h3>
+                        <h3> {city.state} </h3>
+                        <h3> {city.country} </h3>
+                        <h4> {city.report} </h4>
+                        <img src={city.image} alt='City Image'/>
+
+                        <button onClick={
+                            e => {
+                                deleteCity(city.id)
+                            }
+                        }>
+                            Delete
+                        </button>
+
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
+    );
+};
+
+const mapStateToProps = state => {
+    // mapStateToProps always needs an argument of state
+    return { cities: state.cities }
+    // connecting to Redux state, and then setting up a key-value pair. The key can be called
+    // in our JSX
 }
+
+const mapDispatchToProps = dispatch => {
+    return {
+        deleteCity: (id) => {dispatch(removeCity(id))}
+        // id is the argument for the function, the function is the value of the key-value pair
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CitiesList);
