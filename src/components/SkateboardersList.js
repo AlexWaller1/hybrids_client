9b489ce735318c6cc9af51d27f1b520fd367f47e
Alex Waller <alex@artofalexwaller.com>
2021-06-11 03:08:20 -0400

    SkateboardersList

--- src/components/SkateboardersList.js
+++ src/components/SkateboardersList.js
@@ -1,6 +1,10 @@
 // this is where we will render our Skateboarder Entries
 // we will need mapDispatchToProps and mapStateToProps
 // key value pairs will need to be arguments in the function
+import React from 'react'
+import { connect } from 'react-redux'
+import { removeSkateboarder } from '../actions/skateboardersActions'
+import SkateboardersForm from './SkateboardersForm'
 
 const SkateparksList = () => {
 
@@ -8,8 +12,34 @@ const SkateparksList = () => {
         <div>
             <SkateboardersForm />
             <ul>
-                
+                {skateboarders.map(skateboarder => {
+                    <li key={skateboarder.id}>
+                        <h2>{skateboarder.name}</h2>
+                        <h3>{skateboarder.hometown}</h3>
+                        <h3>{skateboarder.style}</h3>
+                        <h4>{skateboarder.biography}</h4>
+                        <img src={skateboarder.image} alt='skateboarder image' />
+                        <button onClick={
+                            e => deleteSkateboarder(skateboarder.id)
+                        }>
+                            Scrub Skateboarder
+                        </button>
+                    </li>
+                })}
+
             </ul>
         </div>
     )
 }
+    mapStateToProps = state => {
+        return { state: state.skateboarders }
+    }
+
+    mapDispatchToProps = dispatch => {
+        return { 
+            deleteSkateboarder: () => {dispatch(removeSkateboarder(id))}
+        }
+    }
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(SkateboardersList);
