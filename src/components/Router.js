469b20344c17ecd56cbdc3981f628219f47842fe
Alex Waller <alex@artofalexwaller.com>
2021-06-16 22:51:07 -0400

    Robots show page and Robots Routing

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,6 +5,7 @@ import Home from '../components/Home'
 import HybridsList from './HybridsList'
 import RobotsList from './RobotsList'
 import SingleHybrid from './SingleHybrid'
+import SingleRobot from './SingleRobot'
 import MinimartsList from './MinimartsList'
 import SkateparksList from './SkateparksList'
 import CitiesList from './CitiesList'
@@ -21,6 +22,7 @@ const Router = () => {
             <Route exact path='/hybrids' component={HybridsList} />
             <Route exact path='/hybrids/:hybridId' component={SingleHybrid} />
             <Route exact path='/robots' component={RobotsList} />
+            <Route exact path='/robots/:robotId' component={SingleRobot} />
             <Route exact path='/minimarts' component={MinimartsList} />
             <Route exact path='/skateparks' component={SkateparksList} />
             <Route exact path='/cities' component={CitiesList} />
--- src/components/SingleRobot.js
+++ src/components/SingleRobot.js
@@ -13,5 +13,42 @@ export const SingleRobot = ({ match }) => {
     const robot = useSelector (state =>
         state.robots.find(robot => robot.id + '' === robotId)
         )
+    // Once we have that robotId, we can use it inside a selector function to find
+    // the right robot object from the Redux store. We know that state.robots should be an
+    // array of all the robot objects, so we can use the Array.find() function to loop
+    // through the array and return the robot entry with the ID we're looking for.
+
+    // It's important to note that the component will re-render any time the value returned
+    // from useSelector changes to a new reference. Components should always try to select the 
+    // smallest possible amount of data they need from the store, which will help ensure that
+    // it only renders when it actually needs to.
+
+    if (!robot) {
+        return (
+            <section>
+                <h2>Robot Not in Database.</h2>
+            </section>
+        )
+    }
+    // It's possible that we might not have a matching post entry in the store - maybe the user
+    // tried to type in the URL directly, or we don't have the right data loaded. If that happens,
+    // the find() function will return undefined instead of an actual post object. Our component
+    // needs to check for that and handle it by showing a 'Post not found!' message in the page.
+
+    return (
+        <section>
+            <article className="robot">
+                <h1> {robot.name} </h1>
+                <h2> {robot.model} </h2>
+                <h3> {robot.manufacturer} </h3>
+                <h4> {robot.purpose} </h4>
+                <img src={robot.image} alt='Robot Image' />
+
+
+            </article>
+        </section>
+    )
 
 }
+
+export default SingleRobot
